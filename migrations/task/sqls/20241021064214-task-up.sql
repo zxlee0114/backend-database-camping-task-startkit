46174ddb684191e1089a64f94d942651c7fb6c0d
zxlee0114 <napoleon.lee0114@gmail.com>
2024-12-08 18:52:01 +0800

    提交：作業1-4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -38,7 +38,8 @@ DELETE FROM "USER"
 WHERE email = 'opacity0@hexschooltest.io';
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
--- 1-4 
+-- 1-4 SELECT COUNT() AS 別名 FROM 資料表 WHERE 條件
+SELECT count(*) AS 所有用戶數量 FROM "USER";
 
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
 
