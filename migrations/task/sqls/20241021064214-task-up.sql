8ebfb1bb824c1128299a030cac7cd8f4572c41da
zxlee0114 <napoleon.lee0114@gmail.com>
2024-12-08 21:05:02 +0800

    提交：作業3-4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -134,6 +134,8 @@ WHERE user_id = (
     WHERE email = 'starplatinum@hexschooltest.io');
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
+insert into "SKILL" (name) values ('空中瑜伽');
+delete from "SKILL" where name = '空中瑜伽';
 
 
 --  ████████  █████   █    █   █ 
