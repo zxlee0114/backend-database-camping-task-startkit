33391cab4a9cd4a7e9963ad9d3bb0484645ee37d
zxlee0114 <napoleon.lee0114@gmail.com>
2024-12-08 20:51:17 +0800

    提交：作業3-2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -104,6 +104,17 @@ INSERT INTO "COACH" (user_id, experience_years) VALUES
     -- 1. 所有教練都有 `重訓` 專長
     -- 2. 教練`肌肉棒子` 需要有 `瑜伽` 專長
     -- 3. 教練`Q太郎` 需要有 `有氧運動` 與 `復健訓練` 專長
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES
+--所有：重訓
+((SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE email='lee2000@hexschooltest.io')),(SELECT id FROM "SKILL" WHERE name='重訓')),
+((SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE email='muscle@hexschooltest.io')),(SELECT id FROM "SKILL" WHERE name='重訓')),
+((SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE email='starplatinum@hexschooltest.io')),(SELECT id FROM "SKILL" WHERE name='重訓')),
+--肌肉棒子：瑜珈
+((SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE email='muscle@hexschooltest.io')),(SELECT id FROM "SKILL" WHERE name='瑜伽')),
+--Q太郎：有氧運動、復健訓練
+((SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE email='starplatinum@hexschooltest.io')),(SELECT id FROM "SKILL" WHERE name='有氧運動')),
+((SELECT id FROM "COACH" WHERE user_id=(SELECT id FROM "USER" WHERE email='starplatinum@hexschooltest.io')),(SELECT id FROM "SKILL" WHERE name='復健訓練'));
+
 
 -- 3-3 修改：更新教練的經驗年數，資料需求如下：
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
